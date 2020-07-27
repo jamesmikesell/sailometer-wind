ebec8766ce8e4eac1eb77fb4f8a69dd09b86f2d2
James <james.mikesell@gmail.com>
2020-07-26 21:14:51 -0400

    feat(ui): switching to stock canvas-gauges, showing true wind angle
    the ng-canvas-gauges wasn't working to update the true wind angle, so switching to the stock package

---
+++ ui/src/app/component/meter/canvas-gauges.d.ts
@@ -0,0 +1,55 @@
+declare class RadialGauge {
+    constructor(config: GaugeConfig);
+    draw(): any;
+}
+
+interface Gauge {
+    options: GaugeConfig;
+    update(config: GaugeConfig): void;
+    value: number;
+
+}
+
+interface GaugeConfig {
+    value?: number;
+    valueText?: string;
+    highlights?: { from: number; to: number; color: string; }[];
+    renderTo?: string;
+    width?: number;
+    height?: number;
+    minValue?: number;
+    maxValue?: number;
+    strokeTicks?: boolean;
+    ticksAngle?: number;
+    majorTicks?: (string | number)[];
+    minorTicks?: number;
+    startAngle?: number;
+    colorMajorTicks?: string;
+    colorMinorTicks?: string;
+    colorTitle?: string;
+    colorUnits?: string;
+    colorNumbers?: string;
+    colorPlate?: string;
+    borderShadowWidth?: number;
+    borders?: boolean;
+    needleType?: "arrow";
+    needleWidth?: number;
+    needleCircleSize?: number;
+    needleCircleOuter?: boolean;
+    needleCircleInner?: boolean;
+    animation?: boolean;
+    colorBorderOuter?: string;
+    colorBorderOuterEnd?: string;
+    colorBorderMiddle?: string;
+    colorBorderMiddleEnd?: string;
+    colorBorderInner?: string;
+    colorBorderInnerEnd?: string;
+    colorNeedleShadowDown?: string;
+    colorNeedleCircleOuter?: string;
+    colorNeedleCircleOuterEnd?: string;
+    colorNeedleCircleInner?: string;
+    colorNeedleCircleInnerEnd?: string;
+    colorValueBoxRect?: string;
+    colorValueBoxRectEnd?: string;
+    valueBox?: boolean;
+}
