3e46368d01bca7801d94108de8c258631a268a83
James Mikesell <james.mikesell@gmail.com>
2021-07-23 21:32:02 -0400

    refactor(): moving bluetooth and gps to calculations to service

--- ui/src/app/component/meter/meter.component.ts
+++ ui/src/app/component/meter/meter.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { InfoService } from 'src/app/service/info.service';
 import { WIND_METER_CONFIG } from './meter-config';
 
 @Component({
@@ -6,22 +9,15 @@ import { WIND_METER_CONFIG } from './meter-config';
   templateUrl: './meter.component.html',
   styleUrls: ['./meter.component.scss']
 })
-export class MeterComponent implements OnInit {
+export class MeterComponent implements OnInit, OnDestroy {
 
-  private serviceId = "e912fa38-f062-4609-b318-9a1fcf116a16";
-  private characteristicId = "20beae71-b0f1-48e4-91c4-594339b68a2b";
-  private decoder = new TextDecoder();
-  private angleOffset = 15.4;
-  private rpmPerKnot = 22.7375;
-  private groundSpeed: number;
+  private destroy = new Subject<void>();
   private dial: Gauge;
-  private unadjustedSensorAngle: number;
   private trueWindMarker = {
     from: -2.5,
     to: 2.5,
     color: "rgba(255, 255, 0, 1)"
   };
-  private connected = false;
   private highlights = [
     {
       from: -45,
@@ -40,7 +36,9 @@ export class MeterComponent implements OnInit {
   trueWindSpeedDisplay = "---";
   apparentWindSpeedDisplay = "---";
 
-  constructor() { }
+  constructor(
+    private infoService: InfoService
+  ) { }
 
   ngOnInit(): void {
     let config = WIND_METER_CONFIG;
@@ -48,124 +46,66 @@ export class MeterComponent implements OnInit {
     config.highlights = this.highlights;
 
     this.dial = new RadialGauge(config).draw();
+
+    this.infoService.connection
+      .pipe(takeUntil(this.destroy))
+      .subscribe(connected => {
+        if (connected) {
+          this.dial.update({ valueBox: false });
+        } else {
+          this.trueWindSpeedDisplay = "---";
+          this.apparentWindSpeedDisplay = "---";
+          this.dial.value = 0;
+          this.dial.update({ valueBox: true });
+          this.clearTrueWindAngle();
+        }
+      });
+
+    this.infoService.windInfo
+      .pipe(takeUntil(this.destroy))
+      .subscribe(windInfo => {
+        this.dial.value = windInfo.apparentWindAngleDegrees;
+        this.apparentWindSpeedDisplay = windInfo.apparentWindSpeedKnots.toFixed(1);
+
+        if (windInfo.trueWindSpeedKnots) {
+          this.trueWindSpeedDisplay = windInfo.trueWindSpeedKnots.toFixed(1);
+
+          let a = windInfo.trueWindAngleDegrees - 2.5;
+          let b = windInfo.trueWindAngleDegrees + 2.5;
+          this.trueWindMarker.from = Math.round(Math.min(a, b));
+          this.trueWindMarker.to = Math.round(Math.max(a, b));
+          this.dial.update({
+            highlights: this.highlights
+          });
+        } else {
+          this.clearTrueWindAngle();
+        }
+      });
+
+    this.infoService.positionInfo
+      .pipe(takeUntil(this.destroy))
+      .subscribe(positionInfo => {
+        this.groundSpeedDisplay = positionInfo.speedOverGroundKnots.toFixed(1);
+        if (positionInfo.headingDegrees != null)
+          this.headingDisplay = positionInfo.headingDegrees.toFixed(0);
+        else
+          this.headingDisplay = "---";
+      });
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
   }
 
   setCenterWindAngle(): void {
-    this.angleOffset = this.unadjustedSensorAngle
+    this.infoService.setCenterWindAngle();
   }
 
   async init(): Promise<void> {
-    if (this.connected)
-      return;
-
-    await this.initBt();
-    this.connected = true;
-    this.initGps();
     navigator.wakeLock.request("screen");
-  }
-
-  private initGps(): void {
-    let options: PositionOptions = {
-      enableHighAccuracy: true,
-      maximumAge: 0
-    };
-
-    navigator.geolocation.watchPosition(position => this.handlePosition(position), undefined, options);
-  }
-
-  private handlePosition(position: GeolocationPosition): void {
-    let knotsPerMeterPerSecond = 1.94384;
-    this.groundSpeed = position.coords.speed * knotsPerMeterPerSecond;
-    this.groundSpeedDisplay = this.groundSpeed.toFixed(1);
-    if (position.coords.heading != null)
-      this.headingDisplay = position.coords.heading.toFixed(0);
-    else
-      this.headingDisplay = "---";
-  }
-
-  private async initBt(): Promise<void> {
-    let config: RequestDeviceOptions = {
-      filters: [
-        {
-          namePrefix: "Sailometer Wind"
-        }
-      ],
-      optionalServices: [this.serviceId]
-    };
-
-
-    try {
-      let device = await navigator.bluetooth.requestDevice(config);
-      this.dial.update({ valueBox: false });
-      device.addEventListener("gattserverdisconnected", event => this.bluetoothDisconnected(event));
-      this.log('Connecting to GATT Server...');
-      let server = await device.gatt.connect();
-      this.log('Getting Service...');
-      let service = await server.getPrimaryService(this.serviceId);
-      this.log('Getting Characteristic...');
-      let characteristic = await service.getCharacteristic(this.characteristicId);
-      await characteristic.startNotifications();
-      this.log('> Notifications started');
-      characteristic.addEventListener('characteristicvaluechanged', event => this.handleBluetoothNotification(event));
-    } catch (error) {
-      this.dial.update({ valueBox: true });
-      this.log(error);
-    }
-  }
-
-  private log(message: string): void {
-    console.log(message);
-  }
-
-  private bluetoothDisconnected(event: Event): void {
-    this.connected = false;
-    this.trueWindSpeedDisplay = "---";
-    this.apparentWindSpeedDisplay = "---";
-    this.dial.value = 0;
-    this.dial.update({ valueBox: true });
-    this.clearTrueWindAngle();
-  }
-
-  private handleBluetoothNotification(event: Event): void {
-    let value: DataView = (event.target as any).value;
-
-    let message = this.decoder.decode(value);
-    let parts = message.split("\t");
-
-    let rotationInterval = Number(parts[1]);
-    let rotationSensorA = Number(parts[2]);
-    let rotationSensorB = Number(parts[3]);
-    let angleRaw = this.degrees(rotationSensorA, rotationSensorB);
-
-    this.unadjustedSensorAngle = ((angleRaw / 1000) * 360 - 180);
-    let apparentWindAngle = this.unadjustedSensorAngle - this.angleOffset;
-    this.dial.value = apparentWindAngle;
-
-    let rpm = 0;
-    if (rotationInterval)
-      rpm = 1 / (rotationInterval / 60000);
-
-    let apparentWindSpeed = (rpm / this.rpmPerKnot);
-    this.apparentWindSpeedDisplay = apparentWindSpeed.toFixed(1);
-
-
-    if (this.groundSpeed != null) {
-      let trueWind = TrueWindCalculations.calculateTrueWind(apparentWindSpeed, apparentWindAngle, this.groundSpeed);
-      if (!isNaN(trueWind.trueWindSpeed)) {
-        this.trueWindSpeedDisplay = trueWind.trueWindSpeed.toFixed(1);
-        let a = trueWind.trueWindAngle - 2.5;
-        let b = trueWind.trueWindAngle + 2.5;
-        this.trueWindMarker.from = Math.round(Math.min(a, b));
-        this.trueWindMarker.to = Math.round(Math.max(a, b));
-        this.dial.update({
-          highlights: this.highlights
-        });
-      } else {
-        this.clearTrueWindAngle();
-      }
-    } else {
-      this.clearTrueWindAngle();
-    }
+    this.infoService.init();
   }
 
   private clearTrueWindAngle(): void {
@@ -175,83 +115,5 @@ export class MeterComponent implements OnInit {
     this.dial.update({ highlights: this.highlights });
   }
 
-  private degrees(a: number, b: number): number {
-    const aMax = 2600;
-    const aMin = 1280;
-    const bMax = aMax;
-    const bMin = aMin;
-
-
-    let aMid = (aMax + aMin) / 2;
-    let bMid = (bMax + bMin) / 2;
-    let aAmplitude = aMax - aMid;
-    let bAmplitude = bMax - bMid;
-
-    a = (a - aMid) / aAmplitude;
-    b = (b - bMid) / bAmplitude;
-
-    let radians = Math.atan(a / b) / Math.PI;
-
-    let angle: number;
-    // Quadrants
-    // 4 1
-    // 3 2
-    if (b >= 0) {
-      //quad 1 or 4
-      if (a >= 0) {
-        //quad 1
-        angle = radians;
-      }
-      else {
-        //quad 4
-        angle = 2 + radians;
-      }
-    }
-    else {
-      //quad 2 or 3
-      angle = 1 + radians;
-    }
-
-    return (angle * 1000 / 2);
-  }
-
 }
 
-class TrueWind {
-  constructor(
-    public trueWindSpeed: number,
-    public trueWindAngle: number
-  ) { }
-}
-
-class TrueWindCalculations {
-  static calculateTrueWind(apparentWindSpeed: number, apparentWindAngle: number, groundSpeed: number): TrueWind {
-    if (apparentWindAngle === 0) {
-      let trueWindSpeed = apparentWindSpeed - groundSpeed;
-      let trueWindAngle = 0;
-      return new TrueWind(trueWindSpeed, trueWindAngle);
-    }
-    if (Math.abs(apparentWindAngle) === 180) {
-      let trueWindSpeed = apparentWindSpeed + groundSpeed;
-      let trueWindAngle = 180;
-      return new TrueWind(trueWindSpeed, trueWindAngle);
-    }
-
-    let aws = apparentWindSpeed;
-    let awa = (180 - Math.abs(apparentWindAngle)) * Math.PI / 180;
-    let sog = groundSpeed;
-    let trueWindSpeed = (aws / Math.sin(awa)) * Math.sin(Math.PI - awa - Math.asin(sog * Math.sin(awa) / aws));
-    /**
-     * Solving for true wind in certain circumstances can result in 2 plausible speeds... I don't think the second value is
-     * value, however this would be how it's calculated
-     *
-     * let trueWindSpeed2 = (aws / Math.sin(awa)) * Math.sin(Math.asin(sog * Math.sin(awa) / aws) - awa);
-     */
-
-    let trueWindAngle = (Math.asin(Math.sin(awa) / aws * sog) / Math.PI * 180) + Math.abs(apparentWindAngle);
-    if (apparentWindAngle < 0)
-      trueWindAngle = trueWindAngle * -1;
-
-    return new TrueWind(trueWindSpeed, trueWindAngle);
-  }
-}
---
+++ ui/src/app/service/info.service.ts
@@ -0,0 +1,227 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoService {
+
+  private serviceId = "e912fa38-f062-4609-b318-9a1fcf116a16";
+  private characteristicId = "20beae71-b0f1-48e4-91c4-594339b68a2b";
+  private decoder = new TextDecoder();
+  private angleOffset = 15.4;
+  private rpmPerKnot = 22.7375;
+  private groundSpeed: number;
+  private unadjustedSensorAngle: number;
+  connection = new BehaviorSubject<boolean>(false);
+  windInfo = new Subject<WindInfo>();
+  positionInfo = new Subject<PositionInfo>();
+
+  constructor() { }
+
+
+  async init(): Promise<void> {
+    if (this.connection.value)
+      return;
+
+    await this.initBt();
+    this.initGps();
+  }
+
+
+  setCenterWindAngle(): void {
+    this.angleOffset = this.unadjustedSensorAngle;
+  }
+
+  private handleBluetoothNotification(event: Event): void {
+    let windInfo = new WindInfo();
+    let value: DataView = (event.target as any).value;
+
+    let message = this.decoder.decode(value);
+    let parts = message.split("\t");
+
+    let rotationInterval = Number(parts[1]);
+    let rotationSensorA = Number(parts[2]);
+    let rotationSensorB = Number(parts[3]);
+    let angleRaw = this.analogSensorToDegrees(rotationSensorA, rotationSensorB);
+
+    this.unadjustedSensorAngle = ((angleRaw / 1000) * 360 - 180);
+    let apparentWindAngle = this.unadjustedSensorAngle - this.angleOffset;
+    windInfo.apparentWindAngleDegrees = apparentWindAngle;
+
+
+    let rpm = 0;
+    if (rotationInterval)
+      rpm = 1 / (rotationInterval / 60000);
+
+    let apparentWindSpeed = (rpm / this.rpmPerKnot);
+    windInfo.apparentWindSpeedKnots = apparentWindSpeed;
+
+
+
+    if (this.groundSpeed != null) {
+      let trueWind = TrueWindCalculations.calculateTrueWind(apparentWindSpeed, apparentWindAngle, this.groundSpeed);
+      if (!isNaN(trueWind.trueWindSpeed)) {
+        windInfo.trueWindSpeedKnots = trueWind.trueWindSpeed;
+        windInfo.trueWindAngleDegrees = trueWind.trueWindAngle;
+      }
+    }
+
+    this.windInfo.next(windInfo);
+  }
+
+  private analogSensorToDegrees(a: number, b: number): number {
+    const aMax = 2600;
+    const aMin = 1280;
+    const bMax = aMax;
+    const bMin = aMin;
+
+
+    let aMid = (aMax + aMin) / 2;
+    let bMid = (bMax + bMin) / 2;
+    let aAmplitude = aMax - aMid;
+    let bAmplitude = bMax - bMid;
+
+    a = (a - aMid) / aAmplitude;
+    b = (b - bMid) / bAmplitude;
+
+    let radians = Math.atan(a / b) / Math.PI;
+
+    let angle: number;
+    // Quadrants
+    // 4 1
+    // 3 2
+    if (b >= 0) {
+      //quad 1 or 4
+      if (a >= 0) {
+        //quad 1
+        angle = radians;
+      }
+      else {
+        //quad 4
+        angle = 2 + radians;
+      }
+    }
+    else {
+      //quad 2 or 3
+      angle = 1 + radians;
+    }
+
+    return (angle * 1000 / 2);
+  }
+
+  private async initBt(): Promise<void> {
+    let config: RequestDeviceOptions = {
+      filters: [
+        {
+          namePrefix: "Sailometer Wind"
+        }
+      ],
+      optionalServices: [this.serviceId]
+    };
+
+
+    try {
+      this.connection.next(true);
+      let device = await navigator.bluetooth.requestDevice(config);
+      device.addEventListener("gattserverdisconnected", event => this.bluetoothDisconnected(event));
+      console.log('Connecting to GATT Server...');
+      let server = await device.gatt.connect();
+      console.log('Getting Service...');
+      let service = await server.getPrimaryService(this.serviceId);
+      console.log('Getting Characteristic...');
+      let characteristic = await service.getCharacteristic(this.characteristicId);
+      await characteristic.startNotifications();
+      console.log('> Notifications started');
+      characteristic.addEventListener('characteristicvaluechanged', event => this.handleBluetoothNotification(event));
+    } catch (error) {
+      this.connection.next(false);
+      console.log(error);
+    }
+  }
+
+
+  private initGps(): void {
+    let options: PositionOptions = {
+      enableHighAccuracy: true,
+      maximumAge: 0
+    };
+
+    navigator.geolocation.watchPosition(position => this.handlePosition(position), undefined, options);
+  }
+
+
+  private bluetoothDisconnected(event: Event): void {
+    this.connection.next(false);
+  }
+
+
+  private handlePosition(position: GeolocationPosition): void {
+    let knotsPerMeterPerSecond = 1.94384;
+    this.groundSpeed = position.coords.speed * knotsPerMeterPerSecond;
+
+    let positionInfo = new PositionInfo(this.groundSpeed, position.coords.heading);
+    this.positionInfo.next(positionInfo);
+  }
+}
+
+
+
+export class WindInfo {
+  constructor(
+    public apparentWindAngleDegrees?: number,
+    public apparentWindSpeedKnots?: number,
+    public trueWindAngleDegrees?: number,
+    public trueWindSpeedKnots?: number,
+  ) { }
+}
+
+
+
+export class PositionInfo {
+  constructor(
+    public speedOverGroundKnots?: number,
+    public headingDegrees?: number,
+  ) { }
+}
+
+
+class TrueWind {
+  constructor(
+    public trueWindSpeed: number,
+    public trueWindAngle: number
+  ) { }
+}
+
+class TrueWindCalculations {
+  static calculateTrueWind(apparentWindSpeed: number, apparentWindAngle: number, groundSpeed: number): TrueWind {
+    if (apparentWindAngle === 0) {
+      let trueWindSpeed = apparentWindSpeed - groundSpeed;
+      let trueWindAngle = 0;
+      return new TrueWind(trueWindSpeed, trueWindAngle);
+    }
+
+    if (Math.abs(apparentWindAngle) === 180) {
+      let trueWindSpeed = apparentWindSpeed + groundSpeed;
+      let trueWindAngle = 180;
+      return new TrueWind(trueWindSpeed, trueWindAngle);
+    }
+
+    let aws = apparentWindSpeed;
+    let awa = (180 - Math.abs(apparentWindAngle)) * Math.PI / 180;
+    let sog = groundSpeed;
+    let trueWindSpeed = (aws / Math.sin(awa)) * Math.sin(Math.PI - awa - Math.asin(sog * Math.sin(awa) / aws));
+    /**
+     * Solving for true wind in certain circumstances can result in 2 plausible speeds... I don't think the second value is
+     * valid, however this would be how it's calculated
+     *
+     * let trueWindSpeed2 = (aws / Math.sin(awa)) * Math.sin(Math.asin(sog * Math.sin(awa) / aws) - awa);
+     */
+
+    let trueWindAngle = (Math.asin(Math.sin(awa) / aws * sog) / Math.PI * 180) + Math.abs(apparentWindAngle);
+    if (apparentWindAngle < 0)
+      trueWindAngle = trueWindAngle * -1;
+
+    return new TrueWind(trueWindSpeed, trueWindAngle);
+  }
+}
